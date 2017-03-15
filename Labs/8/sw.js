bebb3b790fcd2496e919d746d41edc2ec79bf86a
Alex Barragan <abarragan11@cps.edu>
2017-03-15 02:27:00 +0000

    add new folder

---
+++ Labs/8/sw.js
@@ -0,0 +1,21 @@
+/*global caches*/
+importScripts('/cache-polyfill.js');
+window.self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open('airhorner').then(function(cache) {
+return cache.addAll([
+'/',
+'/index.html',
+'/index.html?homescreen=1',
+'/?homescreen=1',
+'/styles/main.css',
+'/scripts/main.min.js',
+'/sounds/airhorn.mp3'
+]);
+})
+);
+});
+window.self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
+});
+
